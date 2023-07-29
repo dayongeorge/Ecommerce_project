ba1da0c092c7b1816b56f2c3a66d5d8c521366ea
dayongeorge <dayongeorge2121@gmail.com>
2023-07-29 19:35:43 +0530

    again changed the static like sreelekshmi

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -133,10 +133,10 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL='/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
